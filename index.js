c415fa6971ca7e7ed8183f4425eb325bdfd42111
getfutureproof-admin <admin@getfutureproof.co.uk>
2021-03-01 00:00:16 +0000

    add executable and scripts

---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+
+const meow = require("meow");
+const translate = require("./pigLatiniser").translate;
+
+const cli = meow(`
+    Usage
+      $ pigLatinise <input>
+    Examples
+      $ pigLatinise 'nix on the stupid'
+      In Pig Latin that's: "ixnay on the upidstay"
+`);
+
+console.log(translate(cli.input[0]))
